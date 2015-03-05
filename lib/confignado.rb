8436bb27eba0f3956c1b376544e0a875d0c208e4
Phil Ostler <github@philostler.com>
2015-03-05 23:33:20 +0000

    #1 Implement basic YAML file loading

--- lib/confignado.rb
+++ lib/confignado.rb
@@ -1,14 +1,25 @@
 module Confignado
-  attr_writer :environment
+  class << self
+    attr_writer :defaults_filename, :environment
 
-  def self.environment
-    @environment || Rail.env || ENV['RACK_ENV']
-  end
+    def defaults_filename
+      @defaults_filename || 'defaults'
+    end
 
-  def self.load(directory)
-    defaults = YAML.load(ERB.new(File.new("#{directory}/default.yml").read).result)
-    environment = YAML.load(ERB.new(File.new("#{directory}/#{self.environment}.yml").read).result)
+    def environment
+      # FIXME: ENV['RAILS_ENV'] is deprecated
+      @environment || ENV['RAILS_ENV'] || ENV['RACK_ENV']
+    end
 
-    final = defaults.merge(environment)
+    def load(directory)
+      defaults = YAML.load(ERB.new(File.new("#{directory}/#{defaults_filename}.yml").read).result)
+      puts defaults.to_s
+      environment = YAML.load(ERB.new(File.new("#{directory}/#{self.environment}.yml").read).result)
+      puts environment.to_s
+
+      # REMOVE: Need to deep merge just-in-time
+      final = defaults.merge(environment)
+      puts final.to_s
+    end
   end
 end
