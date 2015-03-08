79c0aee90e6feaaf59ba1f6d15a540569873a767
Phil Ostler <github@philostler.com>
2015-03-08 13:48:26 +0000

    #1 Load via the concept of file layers instead
    More flexible loaded arrangement

--- lib/confignado.rb
+++ lib/confignado.rb
@@ -1,25 +1,40 @@
 module Confignado
   class << self
-    attr_writer :defaults_filename, :environment
-
-    def defaults_filename
-      @defaults_filename || 'defaults'
-    end
+    attr_writer :environment, :file_layers
 
     def environment
       # FIXME: ENV['RAILS_ENV'] is deprecated
       @environment || ENV['RAILS_ENV'] || ENV['RACK_ENV']
     end
 
-    def load(directory)
-      defaults = YAML.load(ERB.new(File.new("#{directory}/#{defaults_filename}.yml").read).result)
-      puts defaults.to_s
-      environment = YAML.load(ERB.new(File.new("#{directory}/#{self.environment}.yml").read).result)
-      puts environment.to_s
+    def file_layers
+      @file_layers || ['defaults', environment]
+    end
 
-      # REMOVE: Need to deep merge just-in-time
-      final = defaults.merge(environment)
-      puts final.to_s
+    def load(directory)
+      configuration_layers = file_layers.map do |file_layer|
+        YAML.load(ERB.new(File.new("#{directory}/#{file_layer}.yml").read).result)
+      end
+
+      @composite = configuration_layers.reduce do |composite, layer|
+        deep_merge_hashes composite, layer
+      end
+      @composite.freeze
+    end
+    private
+
+    def deep_merge_hashes(hash, other_hash)
+      other_hash.each_pair do |key, value|
+        this_value = hash[key]
+
+        hash[key] = if this_value.is_a?(Hash) && value.is_a?(Hash)
+          this_value.deep_merge(value)
+        else
+          value
+        end
+      end
+
+      hash
     end
   end
 end
